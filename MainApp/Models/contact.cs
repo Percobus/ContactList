48fc2741b13844fd33b8f2d2cb8afa2334c74936
Filip Persson <filip@percobus.se>
2024-12-29 12:14:21 +0100

    Skapat en modell för kontakterna

---
+++ MainApp/Models/contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp.Models
+{
+    public class Contact        // Bygger modellen för kontakterna
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+
+        public Contact()
+        {
+            Id = Guid.NewGuid().ToString();             // Ger ett Id ett unikt värde
+        }
+    }
+}
