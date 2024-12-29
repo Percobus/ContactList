fe1ad79c2dfdbddb3bedf4f4e10d373b6d6a9901
Filip Persson <filip@percobus.se>
2024-12-29 13:14:05 +0100

    Byggt upp listhanteraren

---
+++ MainApp/Services/ContactService.cs
@@ -0,0 +1,27 @@
+﻿using MainApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp.Services
+{
+    public class ContactService
+    {
+        private List<Contact> _contacts = [];
+        private readonly FileService _fileService = new();
+
+        public void Add(Contact contact)
+        {
+            _contacts.Add(contact);
+            _fileService.SaveListToFile(_contacts);
+        }
+
+        public IEnumerable<Contact> GetAll()
+        {
+            _contacts = _fileService.LoadListFromFile();
+            return _contacts;
+        }
+    }
+}
---
+++ MainApp/Services/FileService.cs
@@ -0,0 +1,57 @@
+﻿using MainApp.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace MainApp.Services
+{
+    public class FileService
+    {
+        private readonly string _directoryPath;
+        private readonly string _filePath;
+
+        public FileService(string directoryPath = "Data", string filePath = "contactlist.json")
+        {
+            _directoryPath = directoryPath;
+            _filePath = Path.Combine(_directoryPath, filePath);
+        }
+
+        public void SaveListToFile (List<Contact> list)
+        {
+
+            try
+            {
+                if (!Directory.Exists(_directoryPath))
+                    Directory.CreateDirectory(_directoryPath);
+
+                var json = JsonSerializer.Serialize(list);
+
+                File.WriteAllText(_filePath, json);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        public List<Contact> LoadListFromFile()
+        {
+            try
+            {
+                if (!File.Exists(_filePath))
+                    return [];
+
+                var json = File.ReadAllText(_filePath);
+                var list = JsonSerializer.Deserialize<List<Contact>>(json);
+                return list ?? [];
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return[];
+            }
+
+
+        }
+
+    }
+}
