aed302ac28c3af4871390c46caddaba67b476038
Filip Persson <filip@percobus.se>
2025-01-08 20:42:10 +0100

    Kommenterat FileService.cs

--- MainApp/Services/FileService.cs
+++ MainApp/Services/FileService.cs
@@ -9,23 +9,23 @@ namespace MainApp.Services
         private readonly string _directoryPath;
         private readonly string _filePath;
 
-        public FileService(string directoryPath = "Data", string filePath = "contactlist.json")
+        public FileService(string directoryPath = "Data", string filePath = "contactlist.json")             // Konstruktor som sätter standarvärden
         {
             _directoryPath = directoryPath;
             _filePath = Path.Combine(_directoryPath, filePath);
         }
 
-        public void SaveListToFile (List<Contact> list)
+        public void SaveListToFile (List<Contact> list)                                                    // Metod för att spara lista till fil
         {
 
             try
             {
-                if (!Directory.Exists(_directoryPath))
+                if (!Directory.Exists(_directoryPath))                                                    //  Kontrollerar om denna directory finns, annars skapar
                     Directory.CreateDirectory(_directoryPath);
 
                 var json = JsonSerializer.Serialize(list);
 
-                File.WriteAllText(_filePath, json);
+                File.WriteAllText(_filePath, json);                                                       // Skriver JSON till filen
             }
             catch (Exception ex)
             {
@@ -33,21 +33,21 @@ namespace MainApp.Services
             }
         }
 
-        public List<Contact> LoadListFromFile()
+        public List<Contact> LoadListFromFile()                                                         // Metod för att läsa in listan från fil
         {
             try
             {
-                if (!File.Exists(_filePath))
-                    return [];
+                if (!File.Exists(_filePath))                                                            // Om listan inte finns returnera en tom lista
+                    return new List<Contact>();
 
-                var json = File.ReadAllText(_filePath);
+                var json = File.ReadAllText(_filePath);                                                 // Läser in listan som en sträng
                 var list = JsonSerializer.Deserialize<List<Contact>>(json);
-                return list ?? [];
+                return list ?? new List<Contact>();
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
-                return[];
+                return new List<Contact>();
             }
 
 
